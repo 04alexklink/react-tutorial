2d7c6662fe453d902aac249dc3966b5405a87687
04alexklink <alex_klink123@hotmail.co.uk>
2021-04-04 12:54:10 +0100

    Conditional rendering approach 3. Most common approach: ternary operator approach. This approach can be used within the JSX itself

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -10,27 +10,28 @@ class UserGreeting extends Component {
     }
     
     render() {
-      let message;
-      if(this.state.isLoggedIn) {
-        message = <h1>Welcome Alex</h1>
-      } else {
-        message = <h1>Welcome Guest</h1>
-      }
+        // element variable approach to conditional rendering
+    //   let message;
+    //   if(this.state.isLoggedIn) {
+    //     message = <h1>Welcome Alex</h1>
+    //   } else {
+    //     message = <h1>Welcome Guest</h1>
+    //   }
 
-      return <div>{message}</div>
-
+    //   return <div>{message}</div>
+  
+    // If/Else conditional rendering approach to conditional rendering
         // if(this.state.isLoggedIn) {
         //   return <h1>Welcome Alex</h1>
         // } else {
         //   return <h1>Welcome Guest</h1>
         // }
-        // return (
-        //     <div>
-        //         if(this.state.isLoggedIn)
-        //       <h1>Welcome Alex</h1>
-        //       <h1>Welcome Guest</h1>  
-        //     </div>
-        // )
+
+        return this.state.isLoggedIn? (
+          <h1>Welcome Alex</h1>
+         ) : (
+          <h1>Welcome Guest</h1>
+        )
     }
 }
 
